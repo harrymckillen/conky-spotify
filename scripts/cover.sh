fd45d1b18e6bcb0d66fa1dff9e87d7472bab61b3
Harry McKillen <harry@wolfhound.ie>
2020-04-17 14:18:58 +0100

    Fix album cover cleanup script, update cover image retrieval path, and add character limits for song title, artist and album title

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -12,17 +12,20 @@ if [ "$id_new" != "$id_current" ]; then
 	if [ "$cover" == "" ]; then
 
 	    imgurl=`~/.conky/conky-spotify/scripts/imgurl.sh $id_new`
-	    wget -q -O ~/.conky/conky-spotify/covers/$id_new.jpg $imgurl &> /dev/null
-		# clean up covers folder, keeping only the latest X amount, in below example it is 10
+	    wget -q -O ~/.conky/conky-spotify/covers/$id_new.jpeg $imgurl &> /dev/null
+	    # sets last modified date to current time
+	    touch ~/.conky/conky-spotify/covers/$id_new.jpeg
+		# clean up covers folder, keeping only the latest X amount
 	    rm -f `ls -t ~/.conky/conky-spotify/covers/* | awk 'NR>10'`
-	    rm wget-log #wget-logs are accumulated otherwise
+	    # rm wget-log #wget-logs are accumulated otherwise
+	    # rm `ls -t | awk 'NR>5'`
 	    cover=`ls ~/.conky/conky-spotify/covers | grep $id_new`
 	fi
 
 	if [ "$cover" != "" ]; then
-		cp ~/.conky/conky-spotify/covers/$cover ~/.conky/conky-spotify/current/current.jpg
+		cp ~/.conky/conky-spotify/covers/$cover ~/.conky/conky-spotify/current/current.jpeg
 	else
-		cp ~/.conky/conky-spotify/empty.jpg ~/.conky/conky-spotify/current/current.jpg
+		cp ~/.conky/conky-spotify/empty.jpg ~/.conky/conky-spotify/current/current.jpeg
 	fi
 
 	echo $id_new > ~/.conky/conky-spotify/current/current.txt
